366aa5d53b37a146b508ffe74d01cb663e9a8f44
NatanaelRocha17 <70376180+NatanaelRocha17@users.noreply.github.com>
2021-12-30 08:08:10 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 const btn = document.querySelector("#btn-mobile")
 
 function clicou(event){
-    if(event.type === "touchstart") event.preventDefault
+    if(event.type === "click") event.preventDefault
     const nav = document.querySelector("#nav")
     //togle significa adicione caso não tenha, remova caso tenha 
     nav.classList.toggle("active")
 }
 
 btn.addEventListener("click", clicou)
-btn.addEventListener("touchstart", clicou)
